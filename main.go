112997623f24d5f677ff9155f3868964d4b81df3
aidenwang <yidingwang9867@gmail.com>
2022-07-17 19:24:18 -0700

    BASE

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/bradleyfalzon/ghinstallation"
+	"github.com/google/go-github/v35"
+	"github.com/gregjones/httpcache"
+	"github.com/ossf/scorecard"
+	"github.com/rs/zerolog"
+)
+
+func main() {
+
+}
